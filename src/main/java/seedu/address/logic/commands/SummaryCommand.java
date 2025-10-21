f741ceda7492d3bc15bbbf7b52fa4369643276fd
Dylan <152871121+bluemayo@users.noreply.github.com>
2025-10-21 19:06:15 +0800

    Refine summary message

--- src/main/java/seedu/address/logic/commands/SummaryCommand.java
+++ src/main/java/seedu/address/logic/commands/SummaryCommand.java
@@ -5,6 +5,8 @@ import static java.util.Objects.requireNonNull;
 import javafx.collections.ObservableList;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.staff.Staff;
+import seedu.address.model.person.supplier.Supplier;
 
 /**
  * Summarises all important dates in the address book.
@@ -13,24 +15,52 @@ public class SummaryCommand extends Command {
 
     public static final String COMMAND_WORD = "summary";
     public static final String MESSAGE_USAGE = COMMAND_WORD;
-    public static final String MESSAGE_SUCCESS_FORMAT = "Important dates summarised:\n";
+    public static final String MESSAGE_SUCCESS = "Important dates summarised:\n";
 
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        StringBuilder result = new StringBuilder();
+        StringBuilder staffSummary = new StringBuilder();
+        StringBuilder supplierSummary = new StringBuilder();
         ObservableList<Person> list = model.getFilteredPersonList();
         for (Person person : list) {
             switch(person.getDisplayType()) {
             case STAFF:
+                staffSummary.append(formatStaff(person)).append("\n");
+                break;
             case SUPPLIER:
-                result.append(person);
+                supplierSummary.append(formatSupplier(person)).append("\n");
                 break;
             default: // Do nothing
             }
         }
-        return new CommandResult(MESSAGE_SUCCESS_FORMAT + result);
+
+        StringBuilder result = new StringBuilder(MESSAGE_SUCCESS);
+
+        if (!staffSummary.isEmpty()) {
+            result.append("Staff's shift:\n").append(staffSummary).append("\n");
+        }
+
+        if (!supplierSummary.isEmpty()) {
+            result.append("Supplier's day:\n").append(supplierSummary).append("\n");
+        }
+
+        return new CommandResult(result.toString().trim());
+    }
+
+    private String formatStaff(Person p) {
+        assert p instanceof Staff;
+        Staff staff = (Staff) p;
+
+        return String.format("%s: %s", staff.getName(), staff.getShifts());
+    }
+
+    private String formatSupplier(Person p) {
+        assert p instanceof Supplier;
+        Supplier supplier = (Supplier) p;
+
+        return String.format("%s: %s", supplier.getName(), supplier.getDays());
     }
 }
