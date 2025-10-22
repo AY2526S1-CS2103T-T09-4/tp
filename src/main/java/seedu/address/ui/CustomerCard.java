f946117ed762bc01224d50aedb991acf3585698c
Gabriel Yap <gabrielyap37@gmail.com>
2025-10-22 15:29:17 +0800

    Add loyalty points to GUI

--- src/main/java/seedu/address/ui/CustomerCard.java
+++ src/main/java/seedu/address/ui/CustomerCard.java
@@ -1,5 +1,7 @@
 package seedu.address.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import seedu.address.model.person.Person;
 
 /**
@@ -9,11 +11,16 @@ public class CustomerCard extends PersonCard {
 
     private static final String FXML = "CustomerListCard.fxml";
 
+    @FXML
+    protected Label points;
+
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
     public CustomerCard(Person person, int displayedIndex) {
         super(person, displayedIndex, FXML);
         type.getStyleClass().add("type_customer");
+        points.setText(person.getPoints().toString() + " points");
+
     }
 }
