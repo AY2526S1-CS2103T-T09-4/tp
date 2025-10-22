21202d1a3fb41234de10b387dd7175c16c60fba3
Gabriel Yap <gabrielyap37@gmail.com>
2025-10-22 18:10:50 +0800

    Extend code coverage

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -18,7 +18,6 @@ public abstract class AddCommand extends Command {
             + "Format: "
             + "add <customer/staff/supplier>";
 
-
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
 
---
+++ src/test/java/seedu/address/model/util/SampleDataUtilTest.java
@@ -0,0 +1,33 @@
+package seedu.address.model.util;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.person.Person;
+
+public class SampleDataUtilTest {
+
+    @Test
+    public void getSamplePersons_returnsNonEmptyArray() {
+        Person[] samplePersons = SampleDataUtil.getSamplePersons();
+        assertNotNull(samplePersons);
+        assertTrue(samplePersons.length > 0);
+    }
+
+    @Test
+    public void getSamplePersons_containsValidPersons() {
+        Person[] samplePersons = SampleDataUtil.getSamplePersons();
+        for (Person person : samplePersons) {
+            assertTrue(person.getDisplayType().equals(Person.ContactType.CUSTOMER));
+            assertNotNull(person.getName());
+            assertNotNull(person.getPhone());
+            assertNotNull(person.getEmail());
+            assertNotNull(person.getAddress());
+            assertNotNull(person.getPoints());
+            assertNotNull(person.getTags());
+            assertNotNull(person.getNote());
+        }
+    }
+}
