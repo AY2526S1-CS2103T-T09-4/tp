ac3dd7d2eaea9c5e2b393d2f65d33c58c0407db3
Gabriel Yap <gabrielyap37@gmail.com>
2025-10-23 15:51:52 +0800

    Fix success message

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -43,8 +43,30 @@ public class Messages {
                 .append(person.getEmail())
                 .append("; Address: ")
                 .append(person.getAddress())
+                .append("; Notes: ")
+                .append(person.getNote())
                 .append("; Tags: ");
+
         person.getTags().forEach(builder::append);
+        switch(person.getDisplayType()) {
+        case CUSTOMER:
+            builder.append("; Points: ")
+                    .append(person.getPoints());
+            break;
+
+        case STAFF:
+            builder.append("; Shifts: ")
+                    .append(person.getShifts());
+            break;
+        case SUPPLIER:
+            builder.append("; items: ")
+                    .append(person.getItems())
+                    .append("; days: ")
+                    .append(person.getDays());
+            break;
+        }
+
+
         return builder.toString();
     }
 
