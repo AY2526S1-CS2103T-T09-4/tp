789b36036541683bbc2462523c595005b7e314c7
biscake <leonut.ljh@gmail.com>
2025-10-30 00:23:13 +0800

    Fix alphabetical sort

--- src/main/java/seedu/address/model/sort/NameComparator.java
+++ src/main/java/seedu/address/model/sort/NameComparator.java
@@ -11,7 +11,21 @@ import seedu.address.model.person.Person;
 public class NameComparator implements Comparator<Person> {
     @Override
     public int compare(Person person1, Person person2) {
-        return person1.getName().fullName.compareTo(person2.getName().fullName);
+        String name1 = person1.getName().fullName;
+        String name2 = person2.getName().fullName;
+
+        char firstChar1 = name1.charAt(0);
+        char firstChar2 = name2.charAt(0);
+        char lowerFirstChar1 = Character.toLowerCase(firstChar1);
+        char lowerFirstChar2 = Character.toLowerCase(firstChar2);
+        String name1Rest = name1.substring(1);
+        String name2Rest = name2.substring(1);
+
+        if (lowerFirstChar1 != lowerFirstChar2) {
+            return name1.compareToIgnoreCase(name2);
+        }
+
+        return (firstChar2 - firstChar1) * name1Rest.compareToIgnoreCase(name2Rest);
     }
 
     @Override
