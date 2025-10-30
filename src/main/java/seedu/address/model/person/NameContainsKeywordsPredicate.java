78b777133c05dd32286a1b7bcd660fb18bb28a81
jongrazi <jonathangraziano11@gmail.com>
2025-10-30 16:41:20 +0800

    Fix find allows substring of name + throws exception when multiple prefixes

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -2,14 +2,15 @@ package seedu.address.model.person;
 
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
-import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
  * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
  */
 public class NameContainsKeywordsPredicate implements Predicate<Person> {
+    private static final int MIN_PREFIX_LEN = 1;
     private final List<String> keywords;
 
     public NameContainsKeywordsPredicate(List<String> keywords) {
@@ -18,22 +19,25 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
 
     @Override
     public boolean test(Person person) {
-        return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+        final String name = person.getName().fullName;
+        return keywords.stream().anyMatch(k -> wordPrefixMatchIgnoreCase(name, k));
     }
+
+    private static boolean wordPrefixMatchIgnoreCase(String haystack, String needle) {
+        if (needle == null) return false;
+        String q = needle.trim();
+        if (q.length() < MIN_PREFIX_LEN) return false; // avoid super-broad 1-char matches
+        // \b = word boundary; we only require the *prefix* after the boundary
+        Pattern p = Pattern.compile("(?i)\\b" + Pattern.quote(q));
+        return p.matcher(haystack).find();
+    }
+
 
     @Override
     public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        if (!(other instanceof NameContainsKeywordsPredicate)) {
-            return false;
-        }
-
-        NameContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (NameContainsKeywordsPredicate) other;
-        return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
+        return other == this
+                || (other instanceof NameContainsKeywordsPredicate
+                && keywords.equals(((NameContainsKeywordsPredicate) other).keywords));
     }
 
     @Override
