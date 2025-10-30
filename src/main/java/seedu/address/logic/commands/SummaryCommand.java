68231c7a5a946f1da479d9a3eefe5d104224a6af
Dylan <152871121+bluemayo@users.noreply.github.com>
2025-10-30 13:33:59 +0800

    Remove redundant square brackets from dates in summary command

--- src/main/java/seedu/address/logic/commands/SummaryCommand.java
+++ src/main/java/seedu/address/logic/commands/SummaryCommand.java
@@ -54,15 +54,18 @@ public class SummaryCommand extends Command {
     private String formatStaff(Person p) {
         assert p instanceof Staff;
         Staff staff = (Staff) p;
+        String shifts = staff.getShifts().toString();
 
-        return String.format("%s: %s", staff.getName(), staff.getShifts());
+        return String.format("%s: %s", staff.getName(), shifts.substring(1, shifts.length() - 1));
     }
 
     private String formatSupplier(Person p) {
         assert p instanceof Supplier;
         Supplier supplier = (Supplier) p;
         String items = supplier.getItems().toString();
+        String days = supplier.getDays().toString();
 
-        return String.format("%s (%s): %s", supplier.getName(), items.substring(1, items.length() - 1), supplier.getDays());
+        return String.format("%s (%s): %s", supplier.getName(),
+                items.substring(1, items.length() - 1), days.substring(1, days.length() - 1));
     }
 }
